36e80f76a2b9479a07ee6973d1abb233cac740a2
curiosity101 <stacey@staceygreenwood.com>
2018-12-05 19:59:35 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 /* 
-Here are some resources for learning more about Javascript:
-https://medium.freecodecamp.org/want-to-learn-javascript-heres-a-free-24-part-course-to-get-you-started-e7777baf86fb
-https://medium.com/@petehouston/top-5-resources-to-learn-javascript-today-a611883542cc
+  Here are some resources for learning more about Javascript:
+  https://medium.freecodecamp.org/want-to-learn-javascript-heres-a-free-24-part-course-to-get-you-started-e7777baf86fb
+  https://medium.com/@petehouston/top-5-resources-to-learn-javascript-today-a611883542cc
 
-And a little bit about JavaScript developer interviews
-https://medium.com/dev-bits/a-perfect-guide-for-cracking-a-javascript-interview-a-developers-perspective-23a5c0fa4d0d
-https://medium.com/javascript-scene/10-interview-questions-every-javascript-developer-should-know-6fa6bdf5ad95
+  And a little bit about JavaScript developer interviews
+  https://medium.com/dev-bits/a-perfect-guide-for-cracking-a-javascript-interview-a-developers-perspective-23a5c0fa4d0d
+  https://medium.com/javascript-scene/10-interview-questions-every-javascript-developer-should-know-6fa6bdf5ad95
 
-And if you're curious about what jobs are available then the search phrases you would want to search for are: Front end developer, web developer or Javascript developer.
+  And if you're curious about what jobs are available then the search phrases you would want to search for are: Front end developer, web developer or Javascript developer.
 */
 
-const nameChanged = () => {
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+function nameChanged() {
+  /* 
+    This is an Array - An Array is a list of things that are seperated by commas. You can reconise an Array by the [] that 
+    surrounds the list of things. Each thing in this array is a name, each name is made up of text and therefore it is 
+    classed as a String. You can tell they are strings because each name is surrounded by "" This array is an array of all 
+    of the possible elf names our app can return. We have stored it in a variable called 'elfFirstNames' ready for us to use 
+    later.
+    
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+  */
   let elfFirstNames = [
     "Tinsel",
     "Snowball",
@@ -37,7 +45,10 @@ const nameChanged = () => {
     "Baulbles"
   ];
 
-  // Get the users input
+  /*
+    This code will find the text input on the page and get the current value of it. ie. Whatever has been typed into it. 
+    It then stores it in a variable called 'enteredName' ready for us to use later.
+  */
   let enteredName = document.getElementById("enteredName").value;
 
   // Split up the input by spaces - ['firstName', 'lastName', 'other..']
@@ -58,13 +69,24 @@ const nameChanged = () => {
     }
   }
 
+  // Create the elfName variable, ready to use later
   let elfName = "";
 
-  // Store first & last name lengths
+  /* 
+    Here we store the length of the name the user typed into the input. The length is referring to how many letters are 
+    in the name
+  */
   let firstNameLength = firstName.length;
   let lastNameLength = lastName.length;
 
-  // Check first name exists
+  /* 
+    'if' is a keyword, keywords are words that are reserved by the coding language and assigned a specific task. 
+    'if' has the task of checking the value that is provided in the () is true or not. If the value is true then it will 
+    run any code inside of the next {}
+    
+    In this case we're checking to see if the first name has one or more letter in it
+    If we didn't do this check and just assumed that the user had typed something into the text input, what would happen?
+  */
   if (firstNameLength) {
     // Find the elf first name & store it in elfName
     elfName += elfFirstNames[firstNameLength - 1];
